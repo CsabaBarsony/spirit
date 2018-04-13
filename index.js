bb6e273d70f4d0e0c4fef533b40acb442f152a11
Bársony Csaba <skylc045@skylc.local>
2018-04-13 18:21:51 +0200

    babel

--- index.js
+++ index.js
@@ -1,48 +1,59 @@
-import {Statechart} from 'scion-core'
+'use strict';
 
-module.exports = spirit = (statechart, opts) => {
-  let sc
+var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
 
-  const options = opts || {}
+var _scionCore = require('scion-core');
 
-  setTimeout(() => {
-    sc = new Statechart({states: statechart})
+module.exports = function (statechart, opts) {
+  var sc = void 0;
 
-    sc.on('onEntry', (state, event = {}) => {
-    const action = {...event}
-  action.type = 'a:entry:' + state
-  options.log && console.log('entry', state, action)
-  store.dispatch(action)
-})
+  var options = opts || {};
 
-  sc.on('onExit', (state, event = {}) => {
-    const action = {...event}
-  action.type = 'a:exit:' + state
-  options.log && console.log('exit', state, action)
-  store.dispatch(action)
-})
+  setTimeout(function () {
+    sc = new _scionCore.Statechart({ states: statechart });
 
-  sc.on('onTransition', (state, targetIds, stxIdx, event) => {
-    if(event && event.type) {
-    const action = {...event}
-    action.type = 'a:transition:' + event.type
-    options.log && console.log('transition', action)
-    store.dispatch(action)
-  }
-})
+    sc.on('onEntry', function (state) {
+      var event = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : {};
 
-  sc.start()
-}, 0)
+      var action = _extends({}, event);
+      action.type = 'a:entry:' + state;
+      options.log && console.log('entry', state, action);
+      store.dispatch(action);
+    });
 
-  return store => next => action => {
-    if(!action.type.startsWith('a:')) {
-      sc.gen({
-        name: action.type,
-        data: action,
-      })
-    }
-    else {
-      next(action)
-    }
-  }
-}
+    sc.on('onExit', function (state) {
+      var event = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : {};
+
+      var action = _extends({}, event);
+      action.type = 'a:exit:' + state;
+      options.log && console.log('exit', state, action);
+      store.dispatch(action);
+    });
+
+    sc.on('onTransition', function (state, targetIds, stxIdx, event) {
+      if (event && event.type) {
+        var action = _extends({}, event);
+        action.type = 'a:transition:' + event.type;
+        options.log && console.log('transition', action);
+        store.dispatch(action);
+      }
+    });
+
+    sc.start();
+  }, 0);
+
+  return function (store) {
+    return function (next) {
+      return function (action) {
+        if (!action.type.startsWith('a:')) {
+          sc.gen({
+            name: action.type,
+            data: action
+          });
+        } else {
+          next(action);
+        }
+      };
+    };
+  };
+};
